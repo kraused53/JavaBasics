f91d674b2e2f21fc8c04147c61f618914d8f6283
kraused53 <kraused53@gmail.com>
2026-01-16 12:42:01 -0500

    blah

--- JavaBasics/src/main/java/io/github/nathanjrussell/Main.java
+++ JavaBasics/src/main/java/io/github/nathanjrussell/Main.java
@@ -3,9 +3,6 @@ package io.github.nathanjrussell;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-
-
     public static void main(String[] args) {
 
         //demonstrate printing to console (print vs println)
